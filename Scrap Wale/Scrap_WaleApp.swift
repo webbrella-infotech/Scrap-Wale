050a977b9cba7bb15d46e9cc08ad3cac423f5f11
Vivek Raj <Vivekrajraja@gmail.com>
2025-07-17 14:34:43 +0530

    Initial Commit

---
+++ Scrap Wale/Scrap_WaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scrap_WaleApp.swift
+//  Scrap Wale
+//
+//  Created by WEBbrella Infotech on 17/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Scrap_WaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Scrap WaleTests/Scrap_WaleTests.swift
@@ -0,0 +1,17 @@
+//
+//  Scrap_WaleTests.swift
+//  Scrap WaleTests
+//
+//  Created by WEBbrella Infotech on 17/07/25.
+//
+
+import Testing
+@testable import Scrap_Wale
+
+struct Scrap_WaleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
